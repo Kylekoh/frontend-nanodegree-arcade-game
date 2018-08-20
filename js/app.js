c7b9345f0f477282f85e7d0d27500e2e8a4343c6
kyle_koh <ykoh728@gmail.com>
2018-08-20 15:43:31 +0900

    add comment for app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 // Enemies our player must avoid
 var Enemy = function(x, y, dx) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // Set variables appled to each of our Enemy instance.
+    // Initial x, y parameter applied to setting first status of
+    // each of Enemies. 
+    
+    // The dx parameter applied to control the speed of Enemy. 
     this.sprite = 'images/enemy-bug.png';
     this.y = y;
     this.x = x;
@@ -14,20 +14,26 @@ var Enemy = function(x, y, dx) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    // Multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
     this.dt = dt;
 	
+	// In eveny update, update the x parameter to control the
+	// speed of enemies.
     this.x = this.x + this.dx * this.dt;
     
+    // If enemy go off the canvas screen set the x parameter
+    // to start point and set the dx parameter to random number.
 	if (this.x > 505) {
         this.x = -20;
         this.dx = 100 + Math.floor(Math.random() * 220);
     };	
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
 
+    // This is for collision detection.
+    // If player collide with enemies, return player to start point.
     if (this.x + 80 > player.x && this.x < player.x + 80 && this.y == player.y){
-    	alert("YOU LOSE");
     	player.x = 202;
         player.y = 307;
     }
@@ -39,68 +45,55 @@ Enemy.prototype.render = function() {
 };
 
 
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-const playerxBox = 101;
-const playeryBox = 83;
-
+// Set the Player prototype.
 var Player = function(x, y) {
     this.sprite = "images/char-boy.png"
     this.x = x;
     this.y = y;
 };
 
-Player.prototype.update = function(dt) {
-    this.dt = dt;
+Player.prototype.update = function() {
 };
 
+// Draw the player on the screen, required method for game
 Player.prototype.render = function(x, y) {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Set the player movement using handleInput information.
 Player.prototype.handleInput = function (d){
-    
+	// The d parameter is infomation from
+	// 'keyup' event listener.    
     this.d = d;
 
-    // player direction
+    // Set the player direction and
+    // constrain player exist only in the canvas area.
     if(this.d == "left" && this.x > 0) this.x -= 101;
     if(this.d == "up" && this.y > -25) this.y -= 83;
     if(this.d == "right" && this.x < 404) this.x += 101;
     if(this.d == "down" && this.y < 390) this.y += 83;
     
-
+    // If player reach the water, set back to start point.
     if(this.y < 30){
-	    	alert('CONGRATURATIONS!! YOU WON!!');
-	    	player.x = 202;
-        	player.y = 307;
+	    	this.x = 202;
+        	this.y = 307;
     }
 };
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
- 
+// Set the initial position of player.
+// this position will be also used when player reach the water
+// and when collide with enemies. 
 var player = new Player(202, 307);
 
-let allEnemies = [];
+// Set the Enemies y start point.
 let yStartPosition = [58, 141, 224];
 
-
+// Set the initial infomation of enemies.
+let allEnemies = [];
 for (i = 0; i < yStartPosition.length; i ++){
 	allEnemies.push(new Enemy(0, yStartPosition[i], 200));
 }
 
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -110,6 +103,3 @@ document.addEventListener('keyup', function(e) {
     };
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-
-
